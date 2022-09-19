bc05c760082dab84625e0609c8745e6875033972
unknown <vimalkuriensam@gmail.com>
2022-09-19 22:44:48 +0530

    added items model

---
+++ item-service/pkg/models/items.models.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Items struct {
+	ID        primitive.ObjectID `json:"id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	Value     int                `json:"value" bson:"value"`
+	CreatedAt time.Time          `json:"createdAt" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updatedAt" bson:"updated_at"`
+}
