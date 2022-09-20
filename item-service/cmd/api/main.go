f307ee05f6fccad1b8633ce1db2524147bde5e78
unknown <vimalkuriensam@gmail.com>
2022-09-20 14:51:02 +0530

    added rpc connection to item-service

--- item-service/cmd/api/main.go
+++ item-service/cmd/api/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"net/rpc"
 
 	"github.com/vimalkuriensam/item-service/pkg/config"
 	"github.com/vimalkuriensam/item-service/pkg/models"
+	itemrpc "github.com/vimalkuriensam/item-service/pkg/rpc"
 )
 
 const DEFAULT_ENVIRONMENT = "development"
@@ -28,4 +33,15 @@ func main() {
 	}
 	defer db.Disconnect()
 	db.InsertMongoCollections("items")
+	itemCollection := new(itemrpc.ItemCollection)
+	if err := rpc.Register(itemCollection); err != nil {
+		cfg.Logger.Fatal(err)
+	}
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", cfg.Env["rpcport"]))
+	if err != nil {
+		cfg.Logger.Fatal(err)
+	}
+	cfg.Logger.Printf("RPC server listening on port %v\n", cfg.Env["rpcport"])
+	cfg.Logger.Fatal(http.Serve(listener, nil))
 }
